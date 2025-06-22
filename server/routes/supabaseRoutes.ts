01847e43fc3cda77ac50f8fcb5843e04095968de
Helmi Griffiths <t3grhe00@students.oamk.fi>
2025-06-22 20:17:17 +0300

    chore: clean up unused test files and routes

--- server/routes/supabaseRoutes.ts
+++ server/routes/supabaseRoutes.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 import { validateOrgAccess } from '../middlewares/validateOrgAccess';
-import { getAllOrganizations } from '../controllers/supabase/organizationController';
 import { getAssignments, addAssignments } from '../controllers/supabase/assignmentController';
 import { getRoster, addRoster } from '../controllers/supabase/rosterController';
 import { addFeedback, getFeedback } from '../controllers/supabase/feedbackController';
@@ -9,12 +8,6 @@ import { addFeedback, getFeedback } from '../controllers/supabase/feedbackContro
 const router = express.Router();
 
 // the given org in the URL is the organization ID from github
-
-// -- Organization routes --
-// GET organizations
-router.get('/organizations', isAuthenticated, validateOrgAccess, getAllOrganizations);
-// POST organization
-
 
 // -- Assignment routes --
 // GET assignment(s) 
